681f400fbf72f488545ae3becf7d9d43f7663217
xindong888 <xindong888@163.com>
2017-04-03 00:25:56 +0800

    这是一个库

---
+++ frameworks/class1/Class1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lqj
+ * Date: 2017/4/3
+ * Time: 上午12:19
+ */
+
+namespace class1;
+
+
+class Class1
+{
+        function __construct()
+        {
+            echo '我是雷1';
+        }
+}
---
+++ frameworks/class2/class2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lqj
+ * Date: 2017/4/3
+ * Time: 上午12:19
+ */
+
+namespace class2;
+
+
+class class2
+{
+    function __construct()
+    {
+        echo '我是泪2';
+    }
+}
